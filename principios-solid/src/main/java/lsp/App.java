51acd0e5c5b35bdd69a4d76676729c5438dd7e67
Aristides Debortoli Neto <netodebortoli77@gmail.com>
2024-03-26 15:41:01 -0300

    feat: adicionado exemplo principio LS

--- principios-solid/src/main/java/lsp/App.java
+++ principios-solid/src/main/java/lsp/App.java
@@ -1,4 +1,28 @@
 package lsp;
 
+import java.util.List;
+
 public class App {
+    public static void main(String[] args) {
+        ContaCorrente conta1 = new ContaCorrente(new Conta(100, "JONAS"));
+        ContaEstudante conta2 = new ContaEstudante(new Conta(0, "CARLOS"));
+
+        conta1.deposita(100);
+        conta1.rende();
+        conta2.deposita(100);
+        conta2.saca(50);
+
+        System.out.println("Saldo da conta 1: " + conta1.getSaldo());
+        System.out.println("Saldo da conta 2: " + conta2.getSaldo());
+
+        //// Exemplo de Quebra de Contrato
+        // Conta-poupança não rende e sobrescreveu o método render
+        Conta conta3 = new ContaPoupanca(1000.0, "JOAO");
+        Conta conta4 = new Conta(2000.0, "MARIA");
+        List<Conta> contas = List.of(conta3, conta4);
+
+        for (Conta conta : contas) {
+            conta.rende();
+        }
+    }
 }
---
+++ principios-solid/src/main/java/lsp/ContaCorrente.java
@@ -0,0 +1,26 @@
+package lsp;
+
+public class ContaCorrente {
+
+    private Conta conta;
+
+    public ContaCorrente(Conta conta) {
+        this.conta = conta;
+    }
+
+    public void deposita(double valor) {
+        conta.deposita(valor);
+    }
+
+    public void saca(double valor) {
+        conta.saca(valor);
+    }
+
+    public double getSaldo() {
+        return conta.getSaldo();
+    }
+
+    public void rende() {
+        conta.rende();
+    }
+}
