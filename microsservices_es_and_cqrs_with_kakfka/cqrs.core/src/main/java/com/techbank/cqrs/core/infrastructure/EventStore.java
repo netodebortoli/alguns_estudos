d553e30bd90761e99b018bc599a63339cecc6c65
Aristides Debortoli Neto <netodebortoli77@gmail.com>
2024-04-24 16:31:03 -0300

    feat: update project

--- microsservices_es_and_cqrs_with_kakfka/cqrs.core/src/main/java/com/techbank/cqrs/core/infrastructure/EventStore.java
+++ microsservices_es_and_cqrs_with_kakfka/cqrs.core/src/main/java/com/techbank/cqrs/core/infrastructure/EventStore.java
@@ -1,11 +1,10 @@
 package com.techbank.cqrs.core.infrastructure;
 
 import com.techbank.cqrs.core.events.BaseEvent;
-import com.techbank.cqrs.core.events.EventModel;
 
 import java.util.List;
 
 public interface EventStore {
     void saveEvents(String aggregateId, Iterable<BaseEvent> events, int expectedVersion);
-    List<EventModel> getEvents(String aggregateId);
+    List<BaseEvent> getEvents(String aggregateId);
 }
