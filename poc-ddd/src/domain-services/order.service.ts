b726d38611c30aea696ac18f65325611b2a7e405
Aristides D. Neto <netodebortoli77@gmail.com>
2025-08-06 23:19:05 -0300

    feat: comentario

--- poc-ddd/src/domain-services/order.service.ts
+++ poc-ddd/src/domain-services/order.service.ts
@@ -1,5 +1,8 @@
 import Order from "../entities/order";
 
+// Os domain services são responsáveis por regras de negócio que envolvem múltiplas entidades ou agregados.
+// Eles encapsulam a lógica que não pertence a uma única entidade, mas sim ao domínio como um todo.
+// Eles devem ser stateless, ou seja, não devem manter estado entre as chamadas de método.
 export default class OrderService {
     static getTotalAmount(orders: Order[]): number {
         if (!orders || orders.length === 0) throw new Error('Orders must be required to get total')
