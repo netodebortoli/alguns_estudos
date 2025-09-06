6ef9d9464894f3ccd9054d99a86ede452d7f4bd7
Aristides D. Neto <netodebortoli77@gmail.com>
2025-09-05 22:43:44 -0300

    wip

--- poc-ddd/src/domain/product-module/repository/product.repository.ts
+++ poc-ddd/src/domain/product-module/repository/product.repository.ts
@@ -3,6 +3,9 @@ import Product from "../entity/product";
 
 // Repositorio especifico para a entidade Produto.
 // Algumas funcionalidades que o produto for requerer para acessar o banco, deve ser definido nessa interface.
+// O repositorio é responsavel por persistir os dados e construir os objetos do dominio (Produto).
+// O repositorio parte do pressuposto que o estado do objeto persistido/buscado esta valido.
+// É responsabilidade do modelo de domínio (produto) que o estado do objeto persistido/buscado esteja valido.
 export default interface ProductRepository extends Repository<Product> {
     
 }
