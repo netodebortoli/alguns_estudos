d4821989df13fd27585e37e0284705bafc0a7401
Aristides D. Neto <netodebortoli77@gmail.com>
2025-11-26 16:33:50 -0300

    wip

--- poc-microsservices/wallet-core/src/main/java/aristides/dev/walletcore/WalletCoreApplication.java
+++ poc-microsservices/wallet-core/src/main/java/aristides/dev/walletcore/WalletCoreApplication.java
@@ -1,11 +1,25 @@
 package aristides.dev.walletcore;
 
+import aristides.dev.shared.events.EventDispatcher;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WalletCoreApplication {
+
+    private final EventDispatcher dispatcher;
+
+    public WalletCoreApplication(EventDispatcher dispatcher) {
+        this.dispatcher = dispatcher;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WalletCoreApplication.class, args);
+    }
+
+    @PostConstruct
+    public void registerHandlers() {
+//        this.dispatcher.register("CREATED_TRANSACTION", null);
     }
 }
